b7d87f70c9bc805afabf052f3eace0f1417cf443
Vendetta8247 <vendetta8247@gmail.com>
2016-04-15 03:56:44 +0300

    Added connections and layout

--- app/src/main/java/ua/com/vendetta8247/testmate/MainScreen.java
+++ app/src/main/java/ua/com/vendetta8247/testmate/MainScreen.java
@@ -1,18 +1,26 @@
 package ua.com.vendetta8247.testmate;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
 
+    MainScreenAdapter adapter = new MainScreenAdapter();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +30,21 @@ public class MainScreen extends AppCompatActivity {
 
         RecyclerView rw = (RecyclerView) findViewById(R.id.contentList);
 
-        rw.setAdapter(new MainScreenAdapter());
+        rw.setAdapter(adapter);
+        adapter.addItem(new MainCard1("Test"));
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         rw.setLayoutManager(llm);
 
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -41,36 +52,81 @@ public class MainScreen extends AppCompatActivity {
 }
 
 
-class MainScreenAdapter extends RecyclerView.Adapter
+class MainScreenAdapter extends RecyclerView.Adapter<MainScreenAdapter.SuperViewHolder>
 {
+    private List<MainCard1> cardList;
 
     public MainScreenAdapter()
     {
-
+        cardList = new ArrayList<>();
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(SuperViewHolder holder, int position) {
+        MainCard1 card = cardList.get(position);
+        MainScreenItemViewHolder newHolder = (MainScreenItemViewHolder) holder;
+        newHolder.headerText.setText(card.headerText);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cardList.size();
+    }
+
+    public void addItem(MainCard1 card)
+    {
+        cardList.add(card);
+        notifyItemInserted(cardList.size()-1);
+
     }
 
     @Override
     public MainScreenItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        final View itemView = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.main_screen_card_1, parent, false);
+
+        return new MainScreenItemViewHolder(itemView, parent.getContext());
     }
 
 
-    class MainScreenItemViewHolder extends RecyclerView.ViewHolder
+    class MainScreenItemViewHolder extends SuperViewHolder
     {
+        protected TextView headerText;
+        public Context context;
         public MainScreenItemViewHolder(View v, Context context)
         {
-            super(v);
+            super(v, context);
+            this.context = context;
+            headerText = (TextView) v.findViewById(R.id.header_text_main_1);
 
         }
+    }
+
+    class SuperViewHolder extends RecyclerView.ViewHolder
+    {
+        public SuperViewHolder(View v, Context context) {
+            super(v);
+        }
+    }
+}
+
+
+class MainCard1{
+    public String headerText;
+
+    public MainCard1(String headerText)
+    {
+        this.headerText = headerText;
+
+    }
+}
+
+class MainCard2
+{
+    public String insideText;
+
+    public MainCard2(String insideText)
+    {
+        this.insideText = insideText;
     }
 }
